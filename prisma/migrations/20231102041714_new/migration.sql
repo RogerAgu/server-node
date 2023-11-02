5ece2cbb19fdb02af1b139e0eee124e4e7aaaf50
RogerAgu <bobjackson298@gmail.com>
2023-11-01 23:21:04 -0500

    added prisma and tables

---
+++ prisma/migrations/20231102041714_new/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Company` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Department` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Employee` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Company" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Department" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Employee" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
