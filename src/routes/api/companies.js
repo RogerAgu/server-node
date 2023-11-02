2d48248a9fe45609679fe519428d650c283dc958
RogerAgu <bobjackson298@gmail.com>
2023-11-02 01:05:01 -0500

    added departments

--- src/routes/api/companies.js
+++ src/routes/api/companies.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { getCompanies, getCompany, addCompany } from '../../models/companies'
+import { addDepartment } from '../../models/departments'
 
 const router = Router()
 
@@ -15,6 +16,7 @@ router.get('/:id', async (req, res) => {
     res.status(404).send({ msg: 'Company not found' })
   }
 })
+
 router.post('/', async (req, res) => {
   const companyName = req.body.name
   if (companyName) {
@@ -22,6 +24,22 @@ router.post('/', async (req, res) => {
     res.send(company)
   } else {
     res.status(400).send({ msg: 'Company name is required' })
+  }
+})
+
+router.post('/:id', async (req, res) => {
+  const companyId = req.params.id
+  const company = await getCompany(companyId)
+  if (company) {
+    const departmentName = req.body.name
+    if (departmentName) {
+      const department = await addDepartment(departmentName, companyId)
+      res.send(department)
+    } else {
+      res.status(400).send({ msg: 'Department name is required' })
+    }
+  } else {
+    res.status(400).send({ msg: 'Company does not exist required' })
   }
 })
 
