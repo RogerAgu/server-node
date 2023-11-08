c9381105082f29e96c6d16a0a0ae851797d7ed7d
RogerAgu <bobjackson298@gmail.com>
2023-11-08 00:37:44 -0600

    changed indexes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
 // eslint-disable-next-line import/no-extraneous-dependencies
-import basicAuth from 'express-basic-auth'
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
@@ -14,11 +13,7 @@ import { notFound, errorHandler } from './utils/errors'
 const port = process.env.PORT || 3000
 
 const app = express()
-app.use(
-  basicAuth({
-    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
-  }),
-)
+
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(
   cors({
